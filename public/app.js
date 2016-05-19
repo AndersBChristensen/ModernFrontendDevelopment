45283090f74dbf35c8f049c5c9ed365694f4d029
Anders Butzbach Christensen <anderskoch@live.dk>
2016-05-19 09:52:31 +0200

    comiiit

--- public/app.js
+++ public/app.js
@@ -20,13 +20,22 @@ angular.module('MyShop', ['ngRoute'])
 				controller: ShopController, templateUrl: '/mainContent.html'
 			})
 			.when('/product/:id', {
-				controller: function ProductController($scope, $routeParams, Products) {
+				controller: function ProductController($scope, $routeParams, Products, Basket) {
 					//this.product = Products.findById($routeParams.id);
+					Basket.subscribe(function (updatedBasket) {
+		$scope.basket = updatedBasket;
+		$scope.basketCount = $scope.basket.length;
+	})
+
 					var productId = parseInt($routeParams.id)
 					this.productId = $routeParams.id;
 					this.product = Products.find(function(product){
 						return (product.id === productId);
-					}); 
+					});
+					$scope.purchase = function (id) {
+					$scope.basket.push(id);
+						Basket.update($scope.basket);
+					}; 
 				},
 				controllerAs: 'productController',
 				templateUrl: '/product.html'
@@ -61,7 +70,7 @@ angular.module('MyShop', ['ngRoute'])
 	};
 	})
 	.factory('Products', function () {
-		return [{id: 1, name: "Samsung 16gb micro sd", price: 350, image: "http://www.samsung.com/dk/consumer/mobile-devices/smartphones/galaxy-s/galaxy-s7/images/galaxy-s7-edge_gallery_front_white_s3.png"}, {id: 2, name: "Dress", price: 350, image: "http://images.nyandcompany.com/is/image/NewYorkCompany/productlist2/Draped-Wrap-Dress-Petite_06359490_558.jpg"}, {id: 3, name: "Dog", price: 350, image: "https://www.royalcanin.com/~/media/Royal-Canin/Product-Categories/dog-maxi-landing-hero.ashx"}, {id: 4, name: "Tower", price: 350, image: "https://static.dezeen.com/uploads/2014/02/Ribbon-like-design-wins-competition-for-a-broadcast-tower-and-visitor-centre-in-Turkey-_dezeen_1sq.jpg"}, {id: 5, name: "Can", price: 350, image: "http://www.bangkokpost.com/photos_content/large/1/0305/12305-1292956293xyn63jzdry.jpg"}, {id: 6, name: "beer", price: 350, image: "http://www.bravosolutions.com/img13/beer.gif"}];
+		return [{id: 1, name: "Samsung 16gb", price: 350, image: "http://www.samsung.com/dk/consumer/mobile-devices/smartphones/galaxy-s/galaxy-s7/images/galaxy-s7-edge_gallery_front_white_s3.png"}, {id: 2, name: "Dress", price: 350, image: "http://images.nyandcompany.com/is/image/NewYorkCompany/productlist2/Draped-Wrap-Dress-Petite_06359490_558.jpg"}, {id: 3, name: "Dog", price: 350, image: "https://www.royalcanin.com/~/media/Royal-Canin/Product-Categories/dog-maxi-landing-hero.ashx"}, {id: 4, name: "Tower", price: 350, image: "https://static.dezeen.com/uploads/2014/02/Ribbon-like-design-wins-competition-for-a-broadcast-tower-and-visitor-centre-in-Turkey-_dezeen_1sq.jpg"}, {id: 5, name: "Can", price: 350, image: "http://www.bangkokpost.com/photos_content/large/1/0305/12305-1292956293xyn63jzdry.jpg"}, {id: 6, name: "beer", price: 350, image: "http://www.bravosolutions.com/img13/beer.gif"}];
 	})
 	.factory('Basket', function () {
 		var basket = [];
